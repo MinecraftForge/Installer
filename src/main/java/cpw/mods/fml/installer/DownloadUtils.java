b6edaf4d11cac47d093a4d265b8a34c9ab526148
Lex Manos <LexManos@gmail.com>
2013-09-24 21:00:38 -0700

    Changed the compressed structure. We now do per-file checksums in the unpacked libarary. This is to get around certian JVMs which do not format the generated jar file correctly. Also moved from using LZMA to using XZ, LZMA's successor. This was done because I couldn't find a pure java implementation of LZMA's compressor. Added new LibraryPacker class/application to generate the compressed libraries given folders to search.

--- src/main/java/cpw/mods/fml/installer/DownloadUtils.java
+++ src/main/java/cpw/mods/fml/installer/DownloadUtils.java
@@ -1,25 +1,27 @@
 package cpw.mods.fml.installer;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 import java.util.jar.JarOutputStream;
 import java.util.jar.Pack200;
 
 import javax.swing.ProgressMonitor;
 
-import LZMA.LzmaInputStream;
+import org.tukaani.xz.XZInputStream;
+
 import argo.jdom.JsonNode;
 
 import com.google.common.base.Charsets;
@@ -27,7 +29,6 @@ import com.google.common.base.Function;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
 import com.google.common.io.CharStreams;
 import com.google.common.io.Files;
@@ -35,7 +36,7 @@ import com.google.common.io.InputSupplier;
 
 public class DownloadUtils {
 
-    private static final String PACK_NAME = ".pack.lzma";
+    private static final String PACK_NAME = ".pack.xz";
     public static int downloadInstalledLibraries(String jsonMarker, File librariesDir, IMonitor monitor, List<JsonNode> libraries, int progress, List<String> grabbed, List<String> bad)
     {
         for (JsonNode library : libraries)
@@ -98,14 +99,10 @@ public class DownloadUtils {
                     try
                     {
                         monitor.setNote(String.format("Unpacking packed file %s",packFile.getName()));
-                        FileOutputStream jarBytes = new FileOutputStream(libPath);
-                        JarOutputStream jos = new JarOutputStream(jarBytes);
-                        LzmaInputStream decompressedPackFile = new LzmaInputStream(new FileInputStream(packFile));
-                        Pack200.newUnpacker().unpack(decompressedPackFile, jos);
-                        jos.close();
-                        jarBytes.close();
+                        unpackLibrary(libPath, Files.toByteArray(packFile));
                         monitor.setNote(String.format("Successfully unpacked packed file %s",packFile.getName()));
                         packFile.delete();
+
                         if (checksumValid(libPath, checksums))
                         {
                             grabbed.add(libName);
@@ -130,11 +127,125 @@ public class DownloadUtils {
     {
         try
         {
-            return checksums == null || checksums.isEmpty() || checksums.contains(Hashing.sha1().hashBytes(Files.toByteArray(libPath)).toString());
+            byte[] fileData = Files.toByteArray(libPath);
+            boolean valid = checksums == null || checksums.isEmpty() || checksums.contains(Hashing.sha1().hashBytes(fileData).toString());
+            if (!valid && libPath.getName().endsWith(".jar"))
+            {
+                valid = validateJar(libPath, fileData, checksums);
+            }
+            return valid;
         }
         catch (IOException e)
         {
             return false;
+        }
+    }
+
+    public static void unpackLibrary(File output, byte[] data) throws IOException
+    {
+        if (output.exists())
+        {
+            output.delete();
+        }
+
+        byte[] decompressed = DownloadUtils.readFully(new XZInputStream(new ByteArrayInputStream(data)));
+        
+        //Snag the checksum signature
+        String end = new String(decompressed, decompressed.length - 4, 4);
+        if (!end.equals("SIGN"))
+        {
+            System.out.println("Unpacking failed, signature missing " + end);
+            return;
+        }
+
+        int x = decompressed.length;
+        int len =
+                ((decompressed[x - 8] & 0xFF)      ) |
+                ((decompressed[x - 7] & 0xFF) << 8 ) |
+                ((decompressed[x - 6] & 0xFF) << 16) |
+                ((decompressed[x - 5] & 0xFF) << 24);
+        byte[] checksums = Arrays.copyOfRange(decompressed, decompressed.length - len - 8, decompressed.length - 8);
+
+        FileOutputStream jarBytes = new FileOutputStream(output);
+        JarOutputStream jos = new JarOutputStream(jarBytes);
+
+        Pack200.newUnpacker().unpack(new ByteArrayInputStream(decompressed), jos);
+
+        jos.putNextEntry(new JarEntry("checksums.sha1"));
+        jos.write(checksums);
+        jos.closeEntry();
+
+        jos.close();
+        jarBytes.close();
+    }
+
+    public static boolean validateJar(File libPath, byte[] data, List<String> checksums) throws IOException
+    {
+        System.out.println("Checking \"" + libPath.getAbsolutePath() + "\" internal checksums");
+
+        HashMap<String, String> files = new HashMap<String, String>();
+        String[] hashes = null;
+        JarInputStream jar = new JarInputStream(new ByteArrayInputStream(data));
+        JarEntry entry = jar.getNextJarEntry();
+        while (entry != null)
+        {
+            byte[] eData = readFully(jar);
+
+            if (entry.getName().equals("checksums.sha1"))
+            {
+                hashes = new String(eData, StandardCharsets.UTF_8).split("\n");
+            }
+
+            if (!entry.isDirectory())
+            {
+                files.put(entry.getName(), Hashing.sha1().hashBytes(eData).toString());
+            }
+            entry = jar.getNextJarEntry();
+        }
+        jar.close();
+        
+        if (hashes != null)
+        {
+            boolean failed = !checksums.contains(files.get("checksums.sha1"));
+            if (failed)
+            {
+                System.out.println("    checksums.sha1 failed validation");
+            }
+            else
+            {
+                System.out.println("    checksums.sha1 validated successfully");
+                for (String hash : hashes)
+                {
+                    if (hash.trim().equals("") || !hash.contains(" ")) continue;
+                    String[] e = hash.split(" ");
+                    String validChecksum = e[0];
+                    String target = e[1];
+                    String checksum = files.get(target);
+    
+                    if (!files.containsKey(target) || checksum == null)
+                    {
+                        System.out.println("    " + target + " : missing");
+                        failed = true;
+                    }
+                    else if (!checksum.equals(validChecksum))
+                    {
+                        System.out.println("    " + target + " : failed (" + checksum + ", " + validChecksum + ")");
+                        failed = true;
+                    }
+                }
+            }
+
+            if (!failed)
+            {
+                System.out.println("    Jar contents validated successfully");
+            }
+
+            return !failed;
+        }
+        else
+        {
+            System.out.println("    checksums.sha1 was not found, validation failed");
+            return false; //Missing checksums
         }
     }
 
@@ -155,6 +266,7 @@ public class DownloadUtils {
         }
 
     }
+
     public static boolean downloadFile(String libName, File libPath, String libURL, List<String> checksums)
     {
         try
@@ -179,7 +291,26 @@ public class DownloadUtils {
             return false;
         }
     }
-    static class URLISSupplier implements InputSupplier<InputStream> {
+
+    public static byte[] readFully(InputStream stream) throws IOException
+    {
+        byte[] data = new byte[4096];
+        ByteArrayOutputStream entryBuffer = new ByteArrayOutputStream();
+        int len;
+        do
+        {
+            len = stream.read(data);
+            if (len > 0)
+            {
+                entryBuffer.write(data, 0, len);
+            }
+        } while (len != -1);
+
+        return entryBuffer.toByteArray();
+    }
+
+    static class URLISSupplier implements InputSupplier<InputStream>
+    {
         private final URLConnection connection;
 
         private URLISSupplier(URLConnection connection)
@@ -193,6 +324,7 @@ public class DownloadUtils {
             return connection.getInputStream();
         }
     }
+
     public static IMonitor buildMonitor()
     {
         if (ServerInstall.headless)
---
+++ src/main/java/cpw/mods/fml/installer/LibraryPacker.java
@@ -0,0 +1,226 @@
+package cpw.mods.fml.installer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Pack200;
+import java.util.jar.Pack200.Packer;
+
+import org.tukaani.xz.LZMA2Options;
+import org.tukaani.xz.XZOutputStream;
+
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+
+public class LibraryPacker
+{
+    private static final boolean DEBUG_SAVE_STAGES = false;
+    private static final ArrayList<String> CHECKSUMS = new ArrayList<String>();
+
+    private static class Stopwatch
+    {
+        public void start(){
+            if (fIsRunning) throw new IllegalStateException("Must stop before calling start again.");
+            fStart = System.currentTimeMillis();
+            fStop = 0;
+            fIsRunning = true;
+        }
+        public void stop() {
+            if (!fIsRunning) throw new IllegalStateException("Cannot stop if not currently running.");
+            fStop = System.currentTimeMillis();
+            fIsRunning = false;
+        }
+        
+        public String toString(){ return length() + " ms"; }
+        public long length() { return fStop - fStart; }
+        private long fStart;
+        private long fStop;
+
+        private boolean fIsRunning;
+    }
+    
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Usage: java cpw.mods.fml.installer.LibraryPacker <path1> [path2...]");
+            System.out.println("This program will walk the supplied paths recursivly and create compressed versions of any .jar file they find.");
+            return;
+        }
+
+        for (String path : args)
+        {
+            walk(new File(path));
+        }
+    }
+    
+    private static void walk(File path) throws IOException
+    {
+        if (path.isDirectory())
+        {
+            for (File child : path.listFiles())
+            {
+                walk(child);
+            }
+        }
+        else if (path.getName().endsWith(".jar"))
+        {
+            byte[] xz = compress(path);
+            //System.exit(0);
+            decompress(path, xz, CHECKSUMS);
+        }
+    }
+
+    private static byte[] compress(File path) throws IOException
+    {
+        File lzma = new File(path.getAbsolutePath() + ".pack.lzma");
+        if (lzma.exists())
+        {
+            System.out.println("Skipping \"" + path.getAbsolutePath() + "\" lzma already exists");
+            return null;
+        }
+
+        System.out.println("Processing: " + path.getAbsolutePath());
+        byte[] raw = Files.toByteArray(path);
+        System.out.println("  Raw:        " + raw.length);
+        byte[] packed = pack(raw, path);
+        System.out.println("  Packed:     " + packed.length);
+        byte[] unpacked = unpack(packed, path);
+        System.out.println("  Unpacked:   " + unpacked.length);
+        byte[] checksums = checksum(unpacked, path);
+        System.out.println("  SHA1:       " + Hashing.sha1().hashBytes(checksums).toString());
+        CHECKSUMS.add(Hashing.sha1().hashBytes(checksums).toString());
+        byte[] xzed = xz(packed, checksums, path);
+        System.out.println("  XZed:       " + xzed.length);
+        return xzed;
+    }
+    
+    private static byte[] checksum(byte[] raw, File path) throws IOException
+    {
+        JarInputStream in = new JarInputStream(new ByteArrayInputStream(raw));
+        
+        StringBuffer checksums = new StringBuffer();
+        
+        JarEntry entry = in.getNextJarEntry();
+        while (entry != null)
+        {
+            if (!entry.isDirectory())
+            {
+                checksums.append(Hashing.sha1().hashBytes(DownloadUtils.readFully(in)).toString()).append(' ').append(entry.getName()).append('\n');
+            }
+            entry = in.getNextJarEntry();
+        }
+        in.close();
+        
+        return checksums.toString().getBytes(StandardCharsets.UTF_8);
+    }
+
+    private static final OutputStream NULL_OUT = new OutputStream()
+    {
+        public void write(int b) throws IOException{}
+    };
+    
+    private static byte[] pack(byte[] data, File path) throws IOException
+    {
+        JarInputStream in = new JarInputStream(new ByteArrayInputStream(data));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        
+        Packer packer = Pack200.newPacker();
+        
+        SortedMap<String, String> props = packer.properties();
+        props.put(Packer.EFFORT, "9");
+        props.put(Packer.KEEP_FILE_ORDER, Packer.TRUE);
+        props.put(Packer.UNKNOWN_ATTRIBUTE, Packer.PASS);
+        
+        final PrintStream err = new PrintStream(System.err);
+        System.setErr(new PrintStream(NULL_OUT));
+        packer.pack(in, out);
+        System.setErr(err);
+        
+        in.close();
+        out.close();
+
+        byte[] packed = out.toByteArray();
+        if (DEBUG_SAVE_STAGES)
+        {
+            Files.write(packed, new File(path.getAbsolutePath() + ".pack"));
+        }
+        return packed;
+    }
+
+    private static byte[] unpack(byte[] data, File path) throws IOException
+    {
+        File output = new File(path.getAbsolutePath() + ".unpacked");
+        if (output.exists())
+        {
+            output.delete();
+        }
+
+        FileOutputStream jar = new FileOutputStream(output);
+        JarOutputStream jos = new JarOutputStream(jar);
+        Pack200.newUnpacker().unpack(new ByteArrayInputStream(data), jos);
+        jos.close();
+        jar.close();
+
+        byte[] unpacked = Files.toByteArray(output);
+
+        if (!DEBUG_SAVE_STAGES)
+        {
+            output.delete();
+        }
+
+        return unpacked;
+    }
+    
+    private static byte[] xz(byte[] data, byte[] checksums, File path) throws IOException
+    {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        
+        LZMA2Options options = new LZMA2Options();
+        options.setPreset(8);
+        XZOutputStream xz = new XZOutputStream(out, options);
+        int x = checksums.length;
+        xz.write(data);
+        xz.write(checksums);
+        xz.write(new byte[]{
+                (byte) (x & 0x000000FF),
+                (byte)((x & 0x0000FF00) >> 8),
+                (byte)((x & 0x00FF0000) >> 16),
+                (byte)((x & 0xFF000000) >> 24)
+        });
+        xz.write("SIGN".getBytes()); //Add our sign to validate
+        xz.close();
+
+        byte[] xzed = out.toByteArray();
+        Files.write(xzed, new File(path.getAbsolutePath() + ".pack.xz"));
+        return xzed;
+    }
+
+    private static void decompress(File path, byte[] data, List<String> checksum) throws IOException
+    {
+        Stopwatch t = new Stopwatch();
+        t.start();
+
+        File output = new File(path.getAbsolutePath() + ".unpacked.test");
+
+        DownloadUtils.unpackLibrary(new File(path.getAbsolutePath() + ".unpacked.test"), data);
+        
+        DownloadUtils.validateJar(output, Files.toByteArray(output), CHECKSUMS);
+        
+        t.stop();
+        System.out.println("  Decompress: " + t.toString());
+    }
+    
+    
+}
