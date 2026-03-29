ca8a686facb41be169453e2fb9ee768f47496fdb
LexManos <LexManos@gmail.com>
2026-03-29 14:04:55 -0700

    Split tests into sub-project and add multi-release example

---
+++ src/main/java/net/minecraftforge/installer/MRTest.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+package net.minecraftforge.installer;
+
+public class MRTest {
+    public static int getVersion() {
+        return 8;
+    }
+}
